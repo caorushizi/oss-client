401057454caf223bfd4dbfd3154b843b798e678e
caorushizi <84996057@qq.com>
2020-01-18 12:21:53 +0800

    init hub.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -5,17 +5,7 @@ function App() {
     return (
         <div className='App'>
             <header className='App-header'>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
+                <p>Edit <code>src/App.js</code> and save to reload.</p>
             </header>
         </div>
     );
