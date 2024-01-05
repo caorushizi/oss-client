57927efd60661650fce101d60afcd728fe8841df
士子☀️ <84996057@qq.com>
2024-01-05 09:42:54 +0800

    test

--- src/layout/App/style.ts
+++ src/layout/App/style.ts
@@ -3,61 +3,6 @@ import { createStyles } from "antd-style";
 export default createStyles(({ token, css }) => ({
   container: css`
     height: 100vh;
-
-    .up,
-    .down {
-    }
-    .up-enter {
-      top: 100vh;
-      opacity: 0;
-    }
-
-    .up-enter-active {
-      top: 0;
-      opacity: 1;
-      transition:
-        top 0.16s,
-        opacity 0.16s ease-in-out;
-    }
-
-    .up-exit {
-      top: 0;
-      opacity: 1;
-    }
-
-    .up-exit-active {
-      top: -100vh;
-      opacity: 0;
-      transition:
-        top 0.16s,
-        opacity 0.16s ease-in-out;
-    }
-
-    .down-enter {
-      top: -100vh;
-      opacity: 0;
-    }
-
-    .down-enter-active {
-      top: 0;
-      opacity: 1;
-      transition:
-        top 0.16s,
-        opacity 0.16s ease-in-out;
-    }
-
-    .down-exit {
-      top: 0;
-      opacity: 1;
-    }
-
-    .down-exit-active {
-      top: 100vh;
-      opacity: 0;
-      transition:
-        top 0.16s,
-        opacity 0.16s ease-in-out;
-    }
   `,
   sider: css`
     width: 225px;
